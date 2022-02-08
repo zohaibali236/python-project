7e14795c06ff3a6e7c05d1c18bf43d3862f6d511
Zohaib <ali4401681@cloud.neduet.edu.pk>
2022-02-08 21:22:51 +0500

    database connection

--- main.py
+++ main.py
@@ -2,13 +2,25 @@
 import tkinter as tk
 import mysql.connector as db
 import ctypes
+from datetime import datetime
 """End of importing modules"""
+
+"""Connecting to DB"""
+
+try:
+    dbHandle = db.connect(
+                host = "localhost",
+                user = "root",
+                password = "",
+                database = ""
+    )
+except db.Error as error:
+    print(error, datetime.now())
 
 """Getting screen size"""
 screenSize = [ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)]
 
-
-
+"""master window #1"""
 masterWindow = tk.Tk()
 
 masterWindow.geometry(f"{screenSize[0]}x{screenSize[1]}")
