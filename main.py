92c33b3cd99cac9a4ed21ee317e29fc79c83272b
JazebJaved468 <98709989+JazebJaved468@users.noreply.github.com>
2022-02-16 15:49:44 +0500

    updated shops managing screen

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ m1 = Tk()
 """window sizes"""
 m1.geometry(f"{screenSize[0]}x{screenSize[1]}")
 
+"""window titles"""
+m1.title("Shopping mall")
 
 def cmd():
 
@@ -80,11 +82,11 @@ def login():
     loginPage.destroy()
 
 
-# cartscreen
-
+# shops items management screen
    # labels
-orange_frame = Frame(m1, bg="orange", width=1524, height=screenSize[1])
+orange_frame = Frame(m1, bg="orange", width=1524, height=788)
 white_frame = Frame(m1, bg="white", width=1220, height=730)
+black_frame = Frame(m1, bg="grey", width=1025, height=310 )
 seller_label = Label(orange_frame, text="SELLER", bg="orange", fg="white", font=("aerial", 30))
 categories_label = Label(orange_frame, text="CATEGORIES", bg="orange", fg="white", font=("aerial", 30))
 manage_products = Label(white_frame, text="MANAGE PRODUCTS", bg="white", fg="orange", font=("aerial", 25, "bold"))
@@ -93,29 +95,137 @@ name = Label(white_frame, text="NAME", bg="white", fg="orange", font=("aerial", 
 quantity = Label(white_frame, text="QUANTITY", bg="white", fg="orange", font=("aerial", 15, "bold"))
 price = Label(white_frame, text="PRICE", bg="white", fg="orange", font=("aerial", 15, "bold"))
 category = Label(white_frame, text="CATEGORY", bg="white", fg="orange", font=("aerial", 15, "bold"))
-product_list = Label(white_frame, text="PRODUCTS LIST", bg="white", fg="orange", font=("aerial", 25, "bold"))
+product_lis = Label(white_frame, text="PRODUCTS LIST", bg="white", fg="orange", font=("aerial", 25, "bold"))
+
+
+# creating combo box
+categories = ["Grocery", "Beverage", "Clothes", "Technology", "Foot wear", "Jewellery", "toys",
+              "Watches", "Electronic Appliances"]
+combo_box = ttk.Combobox(white_frame, value=categories, width=22)
+combo_box.insert(0, "Select")
+
+
+# creating and placing treeview
+col = ("pid", "pname", "pquantity", "price")
+product_list = ttk.Treeview(m1, height=13, columns=col)
+
+product_list.column("#0", width=0, minwidth=0)
+product_list.column("pid", width=200, minwidth=200, anchor=CENTER)
+product_list.column("pname", width=300, minwidth=300, anchor=CENTER)
+product_list.column("pquantity", width=300, minwidth=300, anchor=CENTER)
+product_list.column("price", width=200, anchor=CENTER)
+
+product_list.heading("pid", text="Product ID", anchor=CENTER)
+product_list.heading("pname", text="Product NAME", anchor=CENTER)
+product_list.heading("pquantity", text="Product QUANTITY", anchor=CENTER)
+product_list.heading("price", text="PRICE", anchor=CENTER)
+
+product_list.place(x=426, y=460)
+
+
+def search(event):
+    global val
+    val = event.widget.get()
+    if val == "":
+        combo_box['value'] = categories
+    else:
+        data=[]
+        for categ in categories:
+            if val.lower() in categ.lower():
+                data.append(categ)
+        combo_box['value'] = data
+
+
+var_id = StringVar()
+var_name = StringVar()
+var_quantity= IntVar()
+var_price = IntVar()
+
+    
+def display():
+    dbHandle = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                              r'DBQ=C:\Users\Xmart\Documents\shopping mall.accdb;')
+    cur4 = dbHandle.cursor()
+    global val
+    global all_data
+    cur4.execute(f"select * from {val}")
+    all_data = cur4.fetchall()
+    for row in product_list.get_children():
+        product_list.delete(row)
+    j=0
+    for i in all_data:
+        id = i[0]
+        name = i[1]
+        qty = i[2]
+        pr = i[3]
+        product_list.insert(parent='', index='end', iid=j, values=(id, name, qty, pr))
+        j += 1
+
+    dbHandle.close()
+    
+    
+def add():
+    dbHandle = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                              r'DBQ=C:\Users\Xmart\Documents\shopping mall.accdb;')
+    cur1 = dbHandle.cursor()
+    global val
+    cur1.execute(f"insert into {val}(pid,pname,pquantity,price) "
+                f"values('{var_id.get()}','{var_name.get()}','{var_quantity.get()}','{var_price.get()}')")
+
+    dbHandle.commit()
+    dbHandle.close()
+    display()
+
+def edit():
+    dbHandle = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                              r'DBQ=C:\Users\Xmart\Documents\shopping mall.accdb;')
+    cur2 = dbHandle.cursor()
+    global val
+    cur2.execute(f"update {val} set pname='{var_name.get()}',"
+                f" pquantity='{var_quantity.get()}', price='{var_price.get()}' where pid='{var_id.get()}'")
+
+    dbHandle.commit()
+    dbHandle.close()
+    display()
+    
+    
+def delete():
+    dbHandle = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                              r'DBQ=C:\Users\Xmart\Documents\shopping mall.accdb;')
+    cur3 = dbHandle.cursor()
+    cur3.execute(f"delete from {val} where pid='{var_id.get()}'")
+    dbHandle.commit()
+    dbHandle.close()
+    display()
+    
 
 
    # buttons
-logout_button = Button(orange_frame, text="Logout", bg="orange", fg="white", font=("aerial", 13, "italic"), command = cmd)
+logout_button = Button(orange_frame, text="Logout", bg="orange", fg="white", font=("aerial", 13, "italic"))
 add_button = Button(white_frame, text="ADD", bg="orange", fg="white", width=7, height=1,
-                       font=("aerial", 15, "italic"))
+                       font=("aerial", 15, "italic"), command=add)
 edit_button = Button(white_frame, text="EDIT", bg="orange", fg="white", width=7, height=1,
-                        font=("aerial", 15, "italic"))
+                        font=("aerial", 15, "italic"), command=edit)
 delete_button = Button(white_frame, text="DELETE", bg="orange", fg="white", width=7, height=1,
-                          font=("aerial", 15, "italic"))
-clear_button = Button(white_frame, text="CLEAR", bg="orange", fg="white", width=7, height=1,
-                         font=("aerial", 15, "italic"))
+                          font=("aerial", 15, "italic"), command=delete)
+clear_button = Button(white_frame, text="DISPLAY", bg="orange", fg="white", width=7, height=1,
+                         font=("aerial", 15, "italic"), command=display)
+
 
    # entry boxes
-id_entry = Entry(white_frame, bg="white", bd=3, width=25)
-name_entry = Entry(white_frame, bg="white", bd=3, width=25)
-quantity_entry = Entry(white_frame, bg="white", bd=3, width=25)
-price_entry = Entry(white_frame, bg="white", bd=3, width=25)
+id_entry = Entry(white_frame, bg="white", textvariable=var_id, bd=3, width=25)
+name_entry = Entry(white_frame, bg="white", textvariable=var_name, bd=3, width=25)
+quantity_entry = Entry(white_frame, bg="white", textvariable=var_quantity, bd=3, width=25)
+price_entry = Entry(white_frame, bg="white", textvariable=var_price, bd=3, width=25)
+
+
+
 
 # putting on screen
+
 orange_frame.place(x=3, y=1)
 white_frame.place(x=300, y=50)
+black_frame.place(x=415, y=450)
 seller_label.place(x=5, y=200)
 categories_label.place(x=5, y=250)
 logout_button.place(x=150, y=730)
@@ -133,7 +243,9 @@ add_button.place(x=250, y=270)
 edit_button.place(x=450, y=270)
 delete_button.place(x=650, y=270)
 clear_button.place(x=850, y=270)
-product_list.place(x=475, y=350)
+product_lis.place(x=475, y=350)
+combo_box.place(x=236, y=214)
+combo_box.bind("<KeyRelease>", search)
 
 
 m1.mainloop()
