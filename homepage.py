c84e99cc727b294988b4c41c259fdfcbdf290b1c
Jazeb Javed <jazebjaved52@gmail.com>
2022-02-22 18:41:15 +0500

    homapage finalized

--- homepage.py
+++ homepage.py
@@ -37,22 +37,22 @@ def Showhomepage():
     label.pack()
     #text
     l1= Label(homepagewindow,text="Elegante Shopping Mall" , bg="black", fg="white",padx=15, pady=15,font=("Segoe Script",25,"bold"))
-    l1.pack()
+    l1.place(x=580,y=3)
     #buttons
     login_img=PhotoImage(file=r'images\2.png')
     login=Button(homepagewindow,image=login_img,bg="DodgerBlue4",height=100,width=250, command=loginPageinit)
-    login.place(x=320,y=125)
+    login.place(x=320,y=110)
 
     ourshop_img=PhotoImage(file=r'images\tg1.png')
     ourshop=Button(homepagewindow,image=ourshop_img,bg="DarkGoldenrod2",height=100,width=250, command = ourshopsinit)
-    ourshop.place(x=610,y=125)
+    ourshop.place(x=610,y=110)
 
     aboutus_img=PhotoImage(file=r'images\a1.png')
     aboutus=Button(homepagewindow,image=aboutus_img,bg="white",height=100,width=250,)
-    aboutus.place(x=905,y=125)
+    aboutus.place(x=905,y=110)
 
     contact_us_img=PhotoImage(file=r'images\c1.png')
     contact=Button(homepagewindow,image=contact_us_img,bg="white",height=100,width=250)
-    contact.place(x=1200,y=125)
+    contact.place(x=1200,y=110)
 
     homepagewindow.mainloop()
